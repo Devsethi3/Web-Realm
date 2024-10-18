9ed92d448216b06d898a8a07b7a59b56bf96b7ec
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-10-18 22:32:43 +0530

    Update CallToAction.tsx

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -8,7 +8,7 @@ import { toast } from "react-hot-toast";
 
 const CallToAction = () => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const formRef = useRef<HTMLFormElement>(null); // Form reference
+  const formRef = useRef<HTMLFormElement>(null); // Form 
   const [loading, setLoading] = useState(false); // Loading state
 
   const { scrollYProgress } = useScroll({
